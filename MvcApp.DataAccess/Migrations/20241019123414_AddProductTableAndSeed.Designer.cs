6d25d004d5e9f218b2e3090469f090a90f25c120
rati <rbreg18@freeuni.edu.ge>
2024-10-19 16:36:10 +0400

    Create Product table and seed

--- MvcApp.DataAccess/Migrations/20241019123414_AddProductTableAndSeed.Designer.cs
+++ MvcApp.DataAccess/Migrations/20241019123414_AddProductTableAndSeed.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcApp.DataAccess.Data;
 
@@ -10,9 +11,11 @@ using MvcApp.DataAccess.Data;
 namespace MvcApp.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241019123414_AddProductTableAndSeed")]
+    partial class AddProductTableAndSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MvcApp.Models/Producs.cs
+++ MvcApp.Models/Producs.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 
 
-namespace BulkyBook.Models
+namespace MvcApp.Models
 {
     public class Product
     {
