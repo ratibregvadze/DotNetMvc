9ce750b8aad6957c5b076faa693e81bb0872c335
rati <rbreg18@freeuni.edu.ge>
2024-10-28 19:03:06 +0400

    Add product details page

--- MvcApp/Areas/Customer/Controllers/HomeController.cs
+++ MvcApp/Areas/Customer/Controllers/HomeController.cs
@@ -12,15 +12,21 @@ public class HomeController : Controller
     private readonly IUnitOfWork _unitOfWork;
 
     public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
-        {
-            _logger = logger;
-            _unitOfWork = unitOfWork;
-        }
+    {
+        _logger = logger;
+        _unitOfWork = unitOfWork;
+    }
 
     public IActionResult Index()
     {
         IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
         return View(productList);
+    }
+
+    public IActionResult Details(int productId)
+    {
+        Product product = _unitOfWork.Product.Get(u=>u.Id== productId, includeProperties: "Category");
+        return View(product);
     }
 
     public IActionResult Privacy()
